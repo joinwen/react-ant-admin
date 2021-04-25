fe64158607c60328b566cb941728ba895723a2ff
cywen <18205055638@163.com>
2021-04-25 18:50:21 +0800

    dev 自定义 route

---
+++ src/router/not-match/WithNotMatch.js
@@ -0,0 +1,14 @@
+import { Switch, Route } from "react-router-dom";
+import NotFound from "../../views/exception/not-found/NotFound";
+function WithNotMatch(props) {
+  let children = props.children;
+  return (
+    <Switch>
+      {props.children}
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  )
+};
+export default WithNotMatch;
